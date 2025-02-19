e2ce609e100d4b6eb6d9a82ed4e862eff778fd3b
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-02-19 19:40:11 +0700

    nambahin fitur main lagi

--- tebak-angka.cpp
+++ tebak-angka.cpp
@@ -6,27 +6,31 @@
 using namespace std;
 
 int main(){
-    srand(time(0));
-    int angkaRandom = rand() % 10 + 1;
-    int tebakan;
-    int percobaan = 0;
     char ulang;
     
-
     cout << "===== Game Tebak Angka =====" << endl;
-    do{
-        cout << "Masukkan tebakan mu dari angka 1-10!" << endl;
-        cin >> tebakan;
-        percobaan++;
-
-        if(tebakan < angkaRandom){
-            cout << "Salah! Angkanya tebakanmy terlalu rendah!" << endl;
-        } else if(tebakan > angkaRandom){
-            cout << "Salah! Angka tebakan mu terlalu tinggi!" << endl;
-        } else {
-            cout << "Yeee, tebakan mu benar dipercobaan ke-" << percobaan << "! jawabannya adalah " << angkaRandom << endl;
-        }
-    } while(tebakan != angkaRandom);
+    do {
+        srand(time(0));
+        int angkaRandom = rand() % 10 + 1;
+        int tebakan;
+        int percobaan = 0;    
+        do{
+            cout << "Masukkan tebakan mu dari angka 1-10!" << endl;
+            cin >> tebakan;
+            percobaan++;
+    
+            if(tebakan < angkaRandom){
+                cout << "Salah! Angkanya tebakanmy terlalu rendah!" << endl;
+            } else if(tebakan > angkaRandom){
+                cout << "Salah! Angka tebakan mu terlalu tinggi!" << endl;
+            } else {
+                cout << "Yeee, tebakan mu benar dipercobaan ke-" << percobaan << "! jawabannya adalah " << angkaRandom << endl;
+            }
+        } while(tebakan != angkaRandom);
+        cout << "Mau main lagi?" << endl;
+        cin >> ulang;
+    } while(ulang != 'y' || ulang != 'Y');
+    
 
         return 0;
 }
